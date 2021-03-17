d232becb39e250d3e2b2e72cde905d8e6020c614
MarcBrejner <marc@kaabrejner.dk>
2021-03-17 14:43:42 +0100

    Serialized CarFactory

--- src/CarFactory.java
+++ src/CarFactory.java
@@ -1,10 +1,11 @@
+import java.io.Serializable;
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
-public class CarFactory implements API {
+public class CarFactory implements API, Serializable {
     public static LinkedBlockingQueue<Car> cars = new LinkedBlockingQueue<Car>();
     public static LinkedBlockingQueue<Car> finishedCars = new LinkedBlockingQueue<Car>();
 
